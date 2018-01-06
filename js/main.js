65947bae8063b966c3a437751106aff7ab2a3397
Antoine Chouinard <antoine.brochu@me.com>
2018-01-06 10:32:54 -0500

    wa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("I am being dropped");
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      {
+        title: 'Javascript',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+      },
+      {
+        title: 'Sass',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+      },
+      {
+        title: 'Build something awesome',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+      }
+    ]
+  }
+})
