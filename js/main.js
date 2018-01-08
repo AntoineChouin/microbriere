05ee30a2bf8859d999908e12dc2a9c4f0c994bb3
Antoine Chouinard <antoine.brochu@me.com>
2018-01-07 22:46:52 -0500

    Idem for Json View

--- js/main.js
+++ js/main.js
@@ -1,31 +1,77 @@
 console.log("I am being dropped");
-
 var app4 = new Vue({
-  el: '#app-4',
-  data: {
+  el: '#app-4'
+  , data: {
     todos: [
       {
-        title: 'Build something awesome',
-        descriptif: {
-          textTitle: 'Legendary',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        title: {
+          principale: 'Build something awesome'
+          , secondaire: "Voici un farfadelle, la femelle sauvage"
         }
-      },
-      {
-        title: 'Sass',
-        descriptif: {
-          textTitle: 'Legendary',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        , info: {
+          textI: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sint asperiores, soluta ipsam. Enim ducimus ab voluptatibus a est perferendis, tempora, aliquam dolore reiciendis voluptates excepturi, aperiam veritatis vero sed."
+          , textII: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
         }
-      },
-      {
-        title: 'Build something awesome',
-        descriptif: {
-          textTitle: 'Legendary',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        , descriptif: {
+            amertume: "25"
+          , style: "Pale Ale"
+          , alcool: "4.5%"
+          , verre: "pinte"
+          , veillessement: "4 mois"
         }
+      }
+      , {
+        title: {
+          principale: 'La Tête Rousse'
+          , secondaire: "Pour vous faire tourner la tête"
+        }
+        , info: {
+          textI: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sint asperiores, soluta ipsam. Enim ducimus ab voluptatibus a est perferendis, tempora, aliquam dolore reiciendis voluptates excepturi, aperiam veritatis vero sed.",
+          textII: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        },
+        descriptif: {
+            amertume: "25"
+          , style: "Red Irish Ale"
+          , alcool: "7.5%"
+          , verre: "pinte"
+          , veillessement: "1 mois"
+        }
+      }
+      , {
+        title: {
+          principale: 'Build something awesome'
+          , secondaire: "feaef"
+        }
+        , info: {
+          textI: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sint asperiores, soluta ipsam. Enim ducimus ab voluptatibus a est perferendis, tempora, aliquam dolore reiciendis voluptates excepturi, aperiam veritatis vero sed."
+          , textII: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        }
+        , descriptif: {
+          amertume: "25"
+          , style: "Pale Ale"
+          , alcool: "4.5%"
+          , verre: "pinte"
+          , veillessement: "4 mois"
+        }
+      }
+      , {
+        title: {
+          principale: 'Le trêfle à 4 feuilles'
+          , secondaire: "Pour vous faire tourner la tête"
+        }
+        , info: {
+          textI: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sint asperiores, soluta ipsam. Enim ducimus ab voluptatibus a est perferendis, tempora, aliquam dolore reiciendis voluptates excepturi, aperiam veritatis vero sed."
+          , textII: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, nihil est. Architecto id atque, ea libero est iure omnis ipsa, autem dolore expedita nostrum dolores.'
+        }
+        , descriptif: {
+          amertume: "25"
+          , style: "Quatruple"
+          , alcool: "10.5%"
+          , verre: "Ballon"
+          , veillessement: "15 à 24 mois"
+        }
+      }
 
-      },
 //      {
 //        title: 'Build Meteojs',
 //        textTitle: 'Like Zendor, the Wizard',
